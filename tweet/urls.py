130d672034be3a27400d7f91ea93c67b1db710a0
0sol0 <jdsmy1004@gmail.com>
2022-11-11 12:15:02 +0900

    Feat : 게시글 CRUD close #3

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('tweet/', views.tweet, name='tweet')
+    path('tweet/', views.tweet, name='tweet'),
+     path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -1,9 +1,48 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import TweetModel
+
+def home(request):
+    user = request.user.is_authenticated
+    if user:
+        return redirect('/tweet')
+    else:
+        return redirect('/sign-in')
 
 def tweet(request):
     if request.method == 'GET':
         user = request.user.is_authenticated
         if user:
             return render(request, 'tweet/home.html')
-        else: 
+        else:
             return redirect('/sign-in')
+    elif request.method == 'POST':
+        user = request.user
+        my_tweet = TweetModel()
+        my_tweet.author = user
+        my_tweet.content = request.POST.get('my-content', '')
+        my_tweet.save()
+        return redirect('/tweet')
+
+
+def tweet(request):
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            all_tweet = TweetModel.objects.all().order_by('-created_at')
+            return render(request, 'tweet/home.html', {'tweet': all_tweet})
+        else:
+            return redirect('/sign-in')
+    elif request.method == 'POST':
+        user = request.user
+        my_tweet = TweetModel()
+        my_tweet.author = user
+        my_tweet.content = request.POST.get('my-content', '')
+        my_tweet.save()
+        return redirect('/tweet')
+    
+@login_required
+def delete_tweet(request, id):
+    my_tweet = TweetModel.objects.get(id=id)
+    my_tweet.delete()
+    return redirect('/tweet')
